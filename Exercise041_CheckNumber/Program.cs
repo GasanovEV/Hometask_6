efa17e92a6666ae70fde033d8223a17f06ddc808
Egor Gasanov <Gasanov.EV11@ya.ru>
2023-07-04 14:28:40 +0300

    Exercise041_CheckNumber исправления

--- Exercise041_CheckNumber/Program.cs
+++ Exercise041_CheckNumber/Program.cs
@@ -1,43 +1,25 @@
 ﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, 
 сколько чисел больше 0 ввёл пользователь.*/
 
+int m = GetNumberFromUser("Введите количество чисел: ", "Ошибка ввода");
+int[] numbers = new int[m];
 
-/*Определение параметров массива*/
-int arraySize = GetNumberFromUser("Введите размер массива: ", "Ошибка ввода");
-int minValue = GetNumberFromUser("Введите нижний предел массива: ", "Ошибка ввода");
-int maxValue = GetNumberFromUser("Введите верхний предел массива: ", "Ошибка ввода");
-
-/*вызов метода генерации нового массива */
-int[] array = GetArray(arraySize, minValue, maxValue);
-
-/*Вызов метода , подсчитывающего количество положительных чисел в массиве*/
-int PositiveNumbers = GetPositiveNumbers(array);
-
-
-
-
-Console.WriteLine($"В сгенерированный массиве: [{string.Join(", ", array)}] -> {PositiveNumbers}  положительных чисел");
-
-/*Генерация масива с задаными параметрами*/
-int[] GetArray(int size, int minValue, int maxValue)
+for (int i = 0; i < m; i++)
 {
-    if (minValue > maxValue)
-    {
-        throw new ArgumentException("Минимальное значение не может быть больше максимального значения.");
-    }
-
-    Random random = new Random();
-    int[] array = new int[size];
-
-    for (int i = 0; i < size; i++)
-    {
-        array[i] = random.Next(minValue, maxValue + 1);
-    }
-
-    return array;
+    numbers[i] = GetNumberFromUser($"Введите число №{i + 1}: ", "Ошибка ввода");
 }
-/*Считывание введенного числа и передача его в переменную, при введении
- не целочисленного значения выводится сообщение об ошибке*/
+
+int count = 0;
+for (int i = 0; i < m; i++)
+{
+    if (numbers[i] > 0)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine($"Количество чисел больше 0: {count}");
+
 int GetNumberFromUser(string message, string errorMessage)
 {
     while (true)
@@ -52,15 +34,4 @@ int GetNumberFromUser(string message, string errorMessage)
             Console.WriteLine(errorMessage);
         }
     }
-}
-
-/*Метод подсчитывает количество положительных чисел*/
-int GetPositiveNumbers(int[] array)
-{
-    int count = 0;
-    for (int i = 0; i < array.Length; i++)
-    {
-        if (array[i] > 0) count++;
-    }
-    return count;
 }
